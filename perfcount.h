d55311ebae17d11ea30d959b7daf1065591bc887
Christian Spoo <spoo@lfbs.rwth-aachen.de>
2012-03-13 13:19:21 +0100

    perfcount: Fixed wrong parameter type in the perfcount API

--- perfcount.h
+++ perfcount.h
@@ -25,16 +25,16 @@
 #define PERFCOUNT_L2        (uint64_t)0xFF000224ull
 #define PERFCOUNT_L3        (uint64_t)0xFF000309ull
 
-#define PERFCOUNT_CORE_PMC(n)   (n & 0xFF)
-#define PERFCOUNT_UNCORE_PMC(n) (n | 0x100)
+#define PERFCOUNT_CORE_PMC(n)   (n & 0x7F)
+#define PERFCOUNT_UNCORE_PMC(n) (n | 0x80)
 
 void perfcount_init(unsigned int counter, uint64_t config);
 uint64_t perfcount_raw(uint8_t event, uint8_t umask);
 
-void perfcount_start(unsigned int counter);
-void perfcount_stop(unsigned int counter);
-void perfcount_reset(unsigned int counter);
-uint64_t perfcount_read(unsigned int counter);
+void perfcount_start(uint8_t counter);
+void perfcount_stop(uint8_t counter);
+void perfcount_reset(uint8_t counter);
+uint64_t perfcount_read(uint8_t counter);
 
 #endif
 
